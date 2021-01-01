ec2e319a8a7c9136d64a1aa8d7eeb227e7c00a89
nikoletabeyska <76749430+nikoletabeyska@users.noreply.github.com>
2021-01-02 00:26:12 +0200

    Helping functions header file

---
+++ Helping_Functions.h
@@ -0,0 +1,8 @@
+#ifndef Helping_Functions
+#include<string>
+void Menu_output();
+void GenerateRandomLetters(int& numberofletters, std::string& letters);
+void NumberofLetters(int& numberofletters);
+void NumberofRounds(int& numberofrounds);
+
+#endif
