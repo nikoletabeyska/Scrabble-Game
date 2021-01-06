9f8a2a12e1ae700f685ea63e5c4457b41cf64663
nikoletabeyska <76749430+nikoletabeyska@users.noreply.github.com>
2021-01-06 17:34:04 +0200

    Added ToLowerCase function declaration

--- Helping_Functions.h
+++ Helping_Functions.h
@@ -5,6 +5,7 @@ void GenerateRandomLetters(int& numberofletters, std::string& letters);
 void NumberofLetters(int& numberofletters);
 void NumberofRounds(int& numberofrounds);
 bool Word_Check(std::string letters, std::string word);
+void ToLowerCase(std::string& line);
 bool Dictionary_Check(std::string word);
 void AppendtoDictionary(std::string new_word);
 #endif
