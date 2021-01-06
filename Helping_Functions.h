2dab1b50ed84351474dd0fd7ff988a307ace04b9
nikoletabeyska <76749430+nikoletabeyska@users.noreply.github.com>
2021-01-06 22:05:33 +0200

    Added declaration of the is_PossibleWord function

--- Helping_Functions.h
+++ Helping_Functions.h
@@ -2,10 +2,11 @@
 #include<string>
 void Menu_output();
 void GenerateRandomLetters(int& numberofletters, std::string& letters);
+void ToLowerCase(std::string& line);
+bool is_PossibleWord(std::string letters);
 void NumberofLetters(int& numberofletters);
 void NumberofRounds(int& numberofrounds);
 bool Word_Check(std::string letters, std::string word);
-void ToLowerCase(std::string& line);
 bool Dictionary_Check(std::string word);
 void AppendtoDictionary(std::string new_word);
 #endif
