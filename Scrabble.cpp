4303e9495815003e3c587b463e2a13b29ff07c00
nikoletabeyska <76749430+nikoletabeyska@users.noreply.github.com>
2021-01-09 01:46:44 +0200

    Added a new option
    Added an option to see a possible word from the letter sequence and refresh the current round.

--- Scrabble.cpp
+++ Scrabble.cpp
@@ -3,37 +3,70 @@
 
 #include <iostream>
 #include<iomanip>
-#include<random>
 #include "Helping_Functions.h"
 using namespace std;
 
 void Main_Game(int& numberofletters, int& numberofrounds) {
 	string word;
 	string letters;
+	string existing_word;
+	bool fail = 0;
+	int failed_round = 0;
 	int points = 0;
 	cout << endl;
+	
+	cout << endl;
 	for (int i = 1; i <= numberofrounds; i++) {
-		cout << setw(10) << "Round " << i << endl;
-		cout << "Available letters: ";
-		GenerateRandomLetters(numberofletters, letters);
+		if (fail) {
+			i = failed_round;
+			fail = 0;
+		}
+		cout << setw(10) << " Round " << i << endl;
+		cout << " Available letters: ";
+		GenerateRandomLetters(numberofletters, letters,existing_word);
 		cout << endl;
-		cout << "Enter your word: ";
+		cout << " Enter your word: ";
 		cin >> word;
+		if (word[0] == '0') {
+			cout << endl;
+			cout << "A possible word is: " << existing_word << endl;
+			cout << endl;
+			fail = 1;
+			failed_round = i;
+			word.erase();
+			letters.erase();
+			continue;
+		}
 		while (!Word_Check(letters, word)) {
 			word.erase();
-			cout << "Invalid input. Try again with: ";
+			cout << " Invalid word. Try again with: ";
 			for (int i = 0; i < letters.length(); i++) cout << letters[i] << " ";
 			cout << endl;
-			cout << "Enter your word: ";
+			cout << " Enter your word: ";
 			cin >> word;
+			if (word[0] == '0') {
+				cout << endl;
+				cout << "A possible word is: " << existing_word << endl;
+				cout << endl;
+				fail = 1;
+				failed_round = i;
+				word.erase();
+				letters.erase(); break;
+				//continue;
+			}
 		}
+		if (fail) continue;
 		points += word.length();
-		cout << "Your points so far are: " << points << endl;
+		if(i!=numberofrounds)cout << " Your points so far are: " << points << endl;
 		letters.erase();
+		cout << endl;
 	}
 	cout << endl;
-	cout << "Your total points are: " << points << endl;
-	cout << "Returning to MENU" << endl;
+	cout << "--->";
+	cout << "Your total points are: " << points<<endl;
+	cout << endl;
+	cout << " *Returning to MENU*" << endl;
+	cout << endl;
 
 }
 int main()
@@ -42,16 +75,25 @@ int main()
 	int numberofletters = 10;
 	int numberofrounds = 10;
 	do {
+		cout << "       SCRABBLE" << endl;
+		cout << endl;
+		cout << "      How to play: " << endl;
+		cout << "*You will be given a combination of letters from which you should make an existing word." << endl;
+		cout << "*If you can't make a word, during one of the rounds, enter '0' to see a possible word." << endl;
+	    cout<<"Then you will be automatically returned to Round 1." << endl;
+		cout << "*If you want to return to the MENU during the game, enter 'M'." << endl;
+		cout << endl;
 		Menu_output();
 		cin >> choice;
 		switch (choice) {
 		case 1:
+			
 			Main_Game(numberofletters, numberofrounds);
 			break;
 
 		case 2:
 			char settingchoice;
-			cout << "Choose a setting you want to change: ";
+			cout << " Choose a setting you want to change (a/b): ";
 			do {
 				cin >> settingchoice;
 				if (settingchoice == 'a') {
@@ -61,8 +103,8 @@ int main()
 					NumberofRounds(numberofrounds);
 				}
 				else {
-					cout << "Incorrect choice" << endl;
-					cout << "Choose again: ";
+					cout << " Incorrect choice" << endl;
+					cout << " Choose a setting you want to change (a/b): ";
 				}
 			} while (settingchoice != 'a' && settingchoice != 'b');
 
@@ -71,16 +113,20 @@ int main()
 		case 3:
 		{
 	    string new_word;
-		cout << "Enter new word to the dictionary: ";
+		cout << " Enter new word to the dictionary: ";
 		cin >> new_word;
 		AppendtoDictionary(new_word);
 		}
 			break;
 
-		case 4:
+		case 4: 
+			cout << "\n Hope to see you again! :)" << endl;
+		    break;
 		default:
-			cout << "Incorrect choice" << endl;
-			cout << "Choose again: ";
+			cout << endl;
+			cout << " Incorrect choice." << endl;
+			cout << endl;
+		
 		}
 	} while (choice != 4);
 	  return 0;
