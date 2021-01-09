f86ae35127a88a5d4777854792950bac69804988
nikoletabeyska <76749430+nikoletabeyska@users.noreply.github.com>
2021-01-09 16:46:36 +0200

    Added "Rules" option in the menu

--- Scrabble.cpp
+++ Scrabble.cpp
@@ -6,7 +6,7 @@
 #include<limits>
 #include "Helping_Functions.h"
 using namespace std;
-
+//executes if you start a new game
 void Main_Game(int& numberofletters, int& numberofrounds) {
 	string word;
 	string letters;
@@ -16,8 +16,8 @@ void Main_Game(int& numberofletters, int& numberofrounds) {
 	bool returntoMENU = 0;
 	int points = 0;
 	cout << endl;
-	cout << endl;
 	for (int i = 1; i <= numberofrounds; i++) {
+		// Executes if the user has chosen to refresh the round with different combination of letters
 		if (fail) {
 			i = failed_round;
 			fail = 0;
@@ -28,47 +28,71 @@ void Main_Game(int& numberofletters, int& numberofrounds) {
 		cout << endl;
 		cout << " Enter your word: ";
 		cin >> word;
+		// If true, shows the user a possible word and continues the loop, refreshing the round
 		if (word[0] == '0') {
-			cout << endl;
-			cout << "A possible word is: " << existing_word << endl;
-			cout << endl;
+			// Displays a possible word and erases the word string and the letters string
+			ShowPossibleWord(word, letters, existing_word);
+			// Boolean value changes as an indicator for a failed round
 			fail = 1;
+			// Stores the number of the failed round
 			failed_round = i;
-			word.erase();
-			letters.erase();
 			continue;
 		}
-		if (word[0] == 'M') break;
+		// If true, breaks the loop and exits the function, then displays the menu again
+		if (word[0] == 'M') {
+			cout << endl;
+			break;
+		}
+		// Continues until a valid word is entered or the input is either '0' or 'M' 
 		while (!Word_Check(letters, word)) {
+			// Erasing the word string every loop
 			word.erase();
+			cout << endl;
 			cout << " Invalid word. Try again with: ";
 			for (int i = 0; i < letters.length(); i++) cout << letters[i] << " ";
 			cout << endl;
 			cout << " Enter your word: ";
 			cin >> word;
+			// If true, shows the user a possible word and exits the while case
 			if (word[0] == '0') {
-				cout << endl;
-				cout << "A possible word is: " << existing_word << endl;
-				cout << endl;
+				// Displays a possible word and erases the word string and the letters string
+				ShowPossibleWord(word, letters, existing_word);
+			    // Boolean value changes as an indicator for a failed round
 				fail = 1;
+				// Stores the number of the failed round
 				failed_round = i;
-				word.erase();
-				letters.erase(); 
 				break;
 			}
+			// If true, exits the while case
 			if (word[0] == 'M') {
+				// Boolean value changes as an indicator for wanted exit of the game
 				returntoMENU = 1;
 				break;
 			}
 		}
-		if (fail) continue;
-		if (returntoMENU) break;
+		// If the user has choosen to refresh the round the loop continues
+		if (fail) {
+			continue;
+		}
+		// If the user has choosen to return to the menu the loop breaks
+		if (returntoMENU) {
+			cout << endl;
+			break;
+		}
+		// Calculating the points
 		points += word.length();
-		if(i!=numberofrounds)cout << " Your points so far are: " << points << endl;
+		// Displays the curent points until the last possible round
+		if (i != numberofrounds) {
+			cout << " Your points so far are: " << points << endl;
+		}
+		// Erases the letters string each round
 		letters.erase();
+		// Erases the word string each round
+		word.erase();
 		cout << endl;
 	}
-	cout << endl;
+
+	//Displays after the last round
 	cout << "--->";
 	cout << "Your total points are: " << points<<endl;
 	cout << endl;
@@ -81,15 +105,11 @@ int main()
 	int choice = 0;
 	int numberofletters = 10;
 	int numberofrounds =10;
-	cout << "       SCRABBLE" << endl;
+	cout << "     SCRABBLE" << endl;
 	cout << endl;
-	cout << "      How to play: " << endl;
-	cout << "*You will be given a combination of letters from which you should make an existing word." << endl;
-	cout << "*If you can't make a word, during one of the rounds, enter '0' to see a possible word." << endl;
-	cout << "Then you will be automatically returned to Round 1." << endl;
-	cout << "*If you want to return to the MENU during the game, enter 'M'." << endl;
-	cout << endl;
+	
 	do {
+		// Displays the menu
 		Menu_output(numberofrounds,numberofletters);
 		cin >> choice;
 		switch (choice) {
@@ -103,14 +123,18 @@ int main()
 			do {
 				cin >> settingchoice;
 				if (settingchoice == 'a') {
+					// Changes the number of letters
 					NumberofLetters(numberofletters);
 				}
 				else if (settingchoice == 'b') {
+					// Changes the number of rounds
 					NumberofRounds(numberofrounds);
 				}
 				else {
 					cout << " Incorrect choice" << endl;
 					cout << " Choose a setting you want to change (a/b): ";
+					cin.clear();
+					cin.ignore(numeric_limits<streamsize>::max(), '\n');
 				}
 			} while (settingchoice != 'a' && settingchoice != 'b');
 
@@ -121,6 +145,7 @@ int main()
 	    string new_word;
 		cout << " Enter new word to the dictionary: ";
 		cin >> new_word;
+		// Adds new word to the dictionary if it doesn't exist yet
 		AppendtoDictionary(new_word);
 		}
 			break;
@@ -128,6 +153,17 @@ int main()
 		case 4: 
 			cout << "\n Hope to see you again! :)" << endl;
 		    break;
+		case 5:
+			cout << endl;
+			cout << "      How to play: " << endl;
+			cout << "- You will be given a combination of letters from which you should make a valid word." << endl;
+			cout << "- If you can't make a word, during one of the rounds, enter '0' to see a possible word." << endl;
+			cout << "  Then you will be given opportunity to continue with another combination of letters." << endl;
+			cout << "- If you want to return to the MENU during the game, enter 'M'." << endl;
+			cout << "- At the end of the game your total points will be displayed. One point equals to one letter of your word each round." << endl;
+			cout << endl;
+			break;
+
 		default:
 			cout << endl;
 			cout << " Incorrect choice." << endl;
