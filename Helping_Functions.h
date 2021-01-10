ed667adfdd7784ecbc7f7779a6c29e125b7fd80f
nikoletabeyska <76749430+nikoletabeyska@users.noreply.github.com>
2021-01-10 20:29:33 +0200

    Rearanged

--- Helping_Functions.h
+++ Helping_Functions.h
@@ -1,12 +1,14 @@
 #ifndef Helping_Functions
 #include<string>
-void Menu_output();
-void GenerateRandomLetters(int& numberofletters, std::string& letters);
-void ToLowerCase(std::string& line);
-bool is_PossibleWord(std::string letters);
+void Menu_output(int numberofrounds,int numberofletters);
 void NumberofLetters(int& numberofletters);
 void NumberofRounds(int& numberofrounds);
+void GenerateRandomLetters(int& numberofletters, std::string& letters,std::string& existing_word);
+bool is_PossibleWord(std::string letters, std::string& existing_word);
+void ShowPossibleWord(std::string& word, std::string& letters, std::string existing_word);
 bool Word_Check(std::string letters, std::string word);
 bool Dictionary_Check(std::string word);
+void ToLowerCase(std::string& line);
+bool isWord(std::string word);
 void AppendtoDictionary(std::string new_word);
 #endif
